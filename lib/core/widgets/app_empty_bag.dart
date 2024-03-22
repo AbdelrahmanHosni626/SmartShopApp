8fa035fccfaba044f3ad45d95fc0073708505ea4
AbdelrahmanHosni626 <abdohosny626@gmail.com>
2024-03-22 11:59:24 +0200

    - Add Cart Screen Empty Bag - Add Cart Screen List View

---
+++ lib/core/widgets/app_empty_bag.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_app/core/helpers/spacing.dart';
+import 'package:smart_app/core/widgets/app_elevated_button.dart';
+import 'package:smart_app/core/widgets/app_text.dart';
+
+class AppEmptyBag extends StatelessWidget {
+  final String imageUrl, title, subTitle, buttonText;
+
+  const AppEmptyBag(
+      {super.key,
+      required this.imageUrl,
+      required this.title,
+      required this.subTitle,
+      required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(top: 50.h),
+        child: Column(
+          children: [
+            Image.asset(
+              imageUrl,
+              height: size.height * 0.35,
+              width: double.infinity,
+            ),
+            const AppText(
+              text: 'Whoops',
+              fontSize: 40,
+              color: Colors.red,
+            ),
+            verticalSpace(20),
+            AppText(
+              text: title,
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+            ),
+            verticalSpace(20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AppText(
+                text:
+                    subTitle,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            verticalSpace(20),
+            AppElevatedButton(
+              text: buttonText,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/cart/ui/cart_screen.dart
+++ lib/features/cart/ui/cart_screen.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:smart_app/core/widgets/app_empty_bag.dart';
+import 'package:smart_app/features/cart/ui/widgets/shopping_basket_listview_item.dart';
+import 'package:smart_app/features/profile/ui/widgets/app_bar_title.dart';
+import 'package:smart_app/generated/assets.dart';
 
 class CartScreen extends StatelessWidget {
+  final bool isEmpty = false;
+
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Screen'),
-      )
-    );
+    return isEmpty
+        ? const Scaffold(
+            body: AppEmptyBag(
+              imageUrl: Assets.bagShoppingBasket,
+              title: 'Your Cart is empty',
+              subTitle:
+                  'Looks like you have not added anything to your cart yet \nGo ahead and start shopping now',
+              buttonText: 'Shop Now',
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              leading: Image.asset(Assets.bagShoppingCart),
+              title: const AppBarTitle(
+                title: 'Shopping Basket',
+                fontSize: 25,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    IconlyLight.delete,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            body: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) => const ShoppingBasketListViewItem(),
+            ),
+          );
   }
 }
